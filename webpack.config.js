25cf806aeb7e32f8c7793e0aa9d969c0f3ce885c
Christian Boyle <mail@christianboyle.com>
2024-12-18 14:36:10 -0600

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    hot: true,
+    open: true,
+    port: 8080
   },
   plugins: [
     new Dotenv()
